50a4b75f9f04d14704bdc9198f878afa44a07c26
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2025-11-09 07:46:38 +0100

    progress

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,14 +6,39 @@ import { Ionicons } from '@expo/vector-icons'
 const TabsLayout = () => {
   return (
     <Tabs
-      screenOptions={{}}
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "green",
+        tabBarStyle: {
+          backgroundColor: "#1e293b",
+          borderTopColor: "yellow",
+          borderTopWidth: 2,
+          height: 80,
+          paddingBottom: 30,
+          paddingTop: 10
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: 800
+        },
+        headerShown: false
+      }}
     >
       <Tabs.Screen
         name='index'
         options={{
           title: "Todos",
-          tabBarIcon: ({color: string, size: number}) => (
-            <Ionicons />
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='flash-outline' size={size} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          title: "Settings",
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='settings' size={size} color={color} />
           )
         }}
       />
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>`Welcome to Hamilton&apos;s mobile expo app`</Text>
+      <Text style={styles.wip}>Hi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "orange",
+    flexDirection: "column"
+  }, 
+  content: {
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  wip: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+})
